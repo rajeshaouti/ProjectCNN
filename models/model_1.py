325310955fab350a564efc9856ca18cfa0907ce2
rajeshaouti <rajeshaouti@gmail.com>
2023-03-27 15:44:47 -0700

    Update model_1.py

--- models/model_1.py
+++ models/model_1.py
@@ -5,40 +5,47 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
+        
         # Input Block
         self.convblock1 = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=32, kernel_size=(3, 3), padding=0, bias=False),
             nn.ReLU()
-        ) # output_size = 26
+        ) # output_size = 30
 
         # CONVOLUTION BLOCK 1
         self.convblock2 = nn.Sequential(
             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=0, bias=False),
             nn.ReLU()
-        ) # output_size = 24
+        ) # output_size = 28
         self.convblock3 = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), padding=0, bias=False),
             nn.ReLU()
-        ) # output_size = 22
+        ) # output_size = 26
 
         # TRANSITION BLOCK 1
-        self.pool1 = nn.MaxPool2d(2, 2) # output_size = 11
+        self.pool1 = nn.MaxPool2d(2, 2) # output_size = 13
         self.convblock4 = nn.Sequential(
             nn.Conv2d(in_channels=128, out_channels=32, kernel_size=(1, 1), padding=0, bias=False),
             nn.ReLU()
-        ) # output_size = 11       # CONVOLUTION BLOCK 2
+        ) # output_size = 13       
+        
+        # CONVOLUTION BLOCK 2
         self.convblock5 = nn.Sequential(
             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=0, bias=False),
             nn.ReLU()
-        ) # output_size = 9
+        ) # output_size = 11
         self.convblock6 = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), padding=0, bias=False),
             nn.ReLU()
-        ) # output_size = 7
+        ) # output_size = 09
+        self.convblock6 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=64, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU()
+        ) # output_size = 07
 
         # OUTPUT BLOCK
         self.convblock7 = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
+            nn.Conv2d(in_channels=64, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
             nn.ReLU()
         ) # output_size = 7
         self.convblock8 = nn.Sequential(
