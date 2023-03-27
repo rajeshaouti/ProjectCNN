5667bec53cd48e06cfe1ace2b73d9b6dc48d90aa
rajeshaouti <rajeshaouti@gmail.com>
2023-03-27 13:26:47 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,6 +5,10 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt 
 
+import torch
+
+from torch.utils.tensorboard import SummaryWriter
+
 def visualize_input_data(train_set,class_names,rows,cols):
 
   torch.manual_seed(42)
@@ -19,7 +23,46 @@ def visualize_input_data(train_set,class_names,rows,cols):
       plt.title(class_names[label])
       plt.axis(False);
 
-import torch
+
+def create_writer(experiment_name: str, 
+                  model_name: str, 
+                  extra: str=None) -> torch.utils.tensorboard.writer.SummaryWriter():
+    """Creates a torch.utils.tensorboard.writer.SummaryWriter() instance saving to a specific log_dir.
+
+    log_dir is a combination of runs/timestamp/experiment_name/model_name/extra.
+
+    Where timestamp is the current date in YYYY-MM-DD format.
+
+    Args:
+        experiment_name (str): Name of experiment.
+        model_name (str): Name of model.
+        extra (str, optional): Anything extra to add to the directory. Defaults to None.
+
+    Returns:
+        torch.utils.tensorboard.writer.SummaryWriter(): Instance of a writer saving to log_dir.
+
+    Example usage:
+        # Create a writer saving to "runs/2022-06-04/data_10_percent/effnetb2/5_epochs/"
+        writer = create_writer(experiment_name="data_10_percent",
+                               model_name="effnetb2",
+                               extra="5_epochs")
+        # The above is the same as:
+        writer = SummaryWriter(log_dir="runs/2022-06-04/data_10_percent/effnetb2/5_epochs/")
+    """
+    from datetime import datetime
+    import os
+
+    # Get timestamp of current date (all experiments on certain day live in same folder)
+    timestamp = datetime.now().strftime("%Y-%m-%d") # returns current date in YYYY-MM-DD format
+
+    if extra:
+        # Create log directory path
+        log_dir = os.path.join("runs", timestamp, experiment_name, model_name, extra)
+    else:
+        log_dir = os.path.join("runs", timestamp, experiment_name, model_name)
+        
+    print(f"[INFO] Created SummaryWriter, saving to: {log_dir}...")
+    return SummaryWriter(log_dir=log_dir)
 
 def save_model(model: torch.nn.Module,
                target_dir: str,
