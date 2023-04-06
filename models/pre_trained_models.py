c819937de88f4dbe5edb13ffcaf78950f50bb31d
rajeshaouti <rajeshaouti@gmail.com>
2023-04-06 01:54:09 -0700

    Update pre_trained_models.py

--- models/pre_trained_models.py
+++ models/pre_trained_models.py
@@ -36,3 +36,34 @@ def create_effnetb2_model(num_classes:int=3,
     )
     
     return model, transforms
+
+  
+  def resnet18(num_classes:int=3, 
+                          seed:int=42):
+    """Creates an resnet18 feature extractor model and transforms.
+
+    Args:
+        num_classes (int, optional): number of classes in the classifier head. 
+            Defaults to 3.
+        seed (int, optional): random seed value. Defaults to 42.
+
+    Returns:
+        model (torch.nn.Module): resnet18 feature extractor model. 
+        transforms (torchvision.transforms): resnet18 image transforms.
+    """
+    # 1, 2, 3. Create resnet18 pretrained weights, transforms and model
+    weights = torchvision.models.ResNet18_Weights.DEFAULT
+    transforms = weights.transforms()
+    model = torchvision.models.resnet18(weights=weights)
+
+    # 4. Freeze all layers in base model
+    for param in model.parameters():
+        param.requires_grad = False
+
+    # 5. Change classifier head with random seed for reproducibility
+    torch.manual_seed(seed)
+    model.fc = nn.Sequential(
+        nn.Linear(in_features=512, out_features=num_classes),
+    )
+    
+    return model, transforms
