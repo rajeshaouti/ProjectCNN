b5a87268f89aaff3d9981a9cc2bfabb76e8430b4
rajeshaouti <rajeshaouti@gmail.com>
2023-04-14 15:01:07 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -91,17 +91,21 @@ def create_writer(experiment_name: str,
     print(f"[INFO] Created SummaryWriter, saving to: {log_dir}...")
     return SummaryWriter(log_dir=log_dir)
 
+"""
+Contains various utility functions for PyTorch model training and saving.
+"""
+import torch
+from pathlib import Path
+
 def save_model(model: torch.nn.Module,
                target_dir: str,
                model_name: str):
     """Saves a PyTorch model to a target directory.
-
     Args:
     model: A target PyTorch model to save.
     target_dir: A directory for saving the model to.
     model_name: A filename for the saved model. Should include
       either ".pth" or ".pt" as the file extension.
-
     Example usage:
     save_model(model=model_0,
                target_dir="models",
@@ -120,3 +124,43 @@ def save_model(model: torch.nn.Module,
     print(f"[INFO] Saving model to: {model_save_path}")
     torch.save(obj=model.state_dict(),
              f=model_save_path)
+    
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item() # torch.eq() calculates where two tensors are equal
+    acc = (correct / len(y_pred)) * 100 
+    return acc
+  
+  def eval_model(model: torch.nn.Module, 
+               data_loader: torch.utils.data.DataLoader, 
+               loss_fn: torch.nn.Module, 
+               accuracy_fn):
+    """Returns a dictionary containing the results of model predicting on data_loader.
+
+    Args:
+        model (torch.nn.Module): A PyTorch model capable of making predictions on data_loader.
+        data_loader (torch.utils.data.DataLoader): The target dataset to predict on.
+        loss_fn (torch.nn.Module): The loss function of model.
+        accuracy_fn: An accuracy function to compare the models predictions to the truth labels.
+
+    Returns:
+        (dict): Results of model making predictions on data_loader.
+    """
+    loss, acc = 0, 0
+    model.eval()
+    with torch.inference_mode():
+        for X, y in data_loader:
+            # Make predictions with the model
+            y_pred = model(X)
+            
+            # Accumulate the loss and accuracy values per batch
+            loss += loss_fn(y_pred, y)
+            acc += accuracy_fn(y_true=y, 
+                                y_pred=y_pred.argmax(dim=1)) # For accuracy, need the prediction labels (logits -> pred_prob -> pred_labels)
+        
+        # Scale loss and acc to find the average loss/acc per batch
+        loss /= len(data_loader)
+        acc /= len(data_loader)
+        
+    return {"model_name": model.__class__.__name__, # only works when model was created with a class
+            "model_loss": loss.item(),
+            "model_acc": acc}
